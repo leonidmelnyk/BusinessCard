9db7838ac696aafd961e76799843c74821c39e53
Leonid <leonid.melnik.ict@gmail.com>
2019-04-01 18:23:57 +0300

    Very raw project structure

---
+++ BusinessCardTests/BusinessCardTests.swift
@@ -0,0 +1,34 @@
+//
+//  BusinessCardTests.swift
+//  BusinessCardTests
+//
+//  Created by Leonid on 4/1/19.
+//  Copyright © 2019 Melnyk. All rights reserved.
+//
+
+import XCTest
+@testable import BusinessCard
+
+class BusinessCardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
